7394ec32c4075b508a4a43977c426766e2580a20
Юлия Корнева <malena80479@mail.ru>
2026-07-14 16:09:24 +0300

    feat: implement CLI greeting in src/cli.js as required for step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { isEven, generateNumber, getGameDescription } from '../src/index.js';
+import { startCli } from '../src/cli.js';
 
-const roundsCount = 3;
-
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-console.log(getGameDescription());
-
-for (let i = 0; i < roundsCount; i += 1) {
-  const number = generateNumber();
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    process.exit(1);
-  }
-}
-
-console.log(`Congratulations, ${name}!`);
+startCli();
