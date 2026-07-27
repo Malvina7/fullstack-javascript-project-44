bd44e28a6e8b1b8e604e45eca694c60fb5476c39
Юлия Корнева <malena80479@mail.ru>
2026-07-27 14:51:53 +0300

    fix: ensure brain-gcd bin entry is valid

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -2,7 +2,4 @@
 import { runGame } from '../src/game-engine.js';
 import game from '../src/games/gcd.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('Find the greatest common divisor of given numbers.');
-
 runGame(game);
